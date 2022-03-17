bde6d6bb9e38fb0c29688a7bac3a772bc49d638e
Niakelle <morak0398@gmail.com>
2022-03-17 17:07:54 +0200

    started project 003 vue

---
+++ 003_vue/something/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <testComponent message="something BIG"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import testComponent from './components/testComponent.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    testComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ 003_vue/something/src/components/testComponent.vue
@@ -0,0 +1,27 @@
+<template>
+	<div class="test-wrapper">
+		<h1>{{ message }}</h1>
+	</div>
+</template>
+
+<script>
+import { defineComponent } from '@vue/composition-api'
+
+export default ({
+	name: 'testComponent',
+	props: {
+		message: String
+	}
+})
+
+</script>
+
+<style scoped>
+	h1 {
+		color: red;
+		font-size: 50px;
+	}
+	.test-wrapper {
+		border: 3px solid black;
+	}
+</style>
