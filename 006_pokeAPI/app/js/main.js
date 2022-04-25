f73889618d9f27d4c0d1268f2810e665cec27e2e
Kayn <morak0398@gmail.com>
2022-04-25 10:30:58 +0300

    working on 006_pokeAPI project

--- 006_pokeAPI/app/js/main.js
+++ 006_pokeAPI/app/js/main.js
@@ -1,18 +1,55 @@
 const button = document.querySelector('.get-new');
 const url = "https://pokeapi.co/api/v2/";
 const getPokemonsUrl = url + "pokemon?limit=5&offset=0";
+const card = document.querySelector('.card');
 
-fetch(getPokemonsUrl)
-	.then(function (response) {
-		console.log('response', response);
-		response.json().then(function (data) {
-			console.log('data', data);
-		})
-	})
 
 async function requestPokemons (event) {
 	event.preventDefault();
-	console.log('click');
+
+	let result;
+	await fetch(getPokemonsUrl)
+	.then(function (response) {
+		if (response.status !== 200) {
+			return Promise.reject(new Error (response.statusText));
+		}
+		return Promise.resolve(response);
+	})
+	.then(function (response) {
+		return response.json();
+	})
+	.then(function (data) {
+		console.log('data', data);
+		result = data;
+	})
+
+	let image = getImage(result);
+
+	card.insertAdjacentHTML('afterbegin', `
+		<img src="${image}" alt="pokemon picture" class="card__img">
+	`)
 }
+
+
+function getImage (object) {
+	let url = object.results[0].url;
+	fetch(url)
+		.then(function (response) {
+			if (response.status !== 200) {
+				return Promise.reject(new Error (response.statusText));
+			}
+			return Promise.resolve(response);
+		})
+		.then(function (response) {
+			return response.json();
+		})
+		.then(function (data) {
+			let calcs = data.sprites.other.official-artwork.front_default;
+			console.log(calcs);
+			return calcs
+		})
+}
+
+// sprites > other > official-artwork > front_default
 
 button.addEventListener("click", requestPokemons);
